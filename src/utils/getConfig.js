a0a4146c3b8ff0396b83ac031345b9896029c566
Toby Wilson <wilsontcl@gmail.com>
2021-01-25 09:24:55 +0000

    getConfigReports

--- src/utils/getConfig.js
+++ src/utils/getConfig.js
@@ -1,6 +1,10 @@
 import store from '../store';
 import filters from '../config.json';
 
+export const getConfigReports = () => {
+  return Object.keys(filters);
+};
+
 export const getConfig = (attr) => {
   const report = store.getState().report;
   return filters[report][attr];
