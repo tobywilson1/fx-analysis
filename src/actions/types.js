bfbbd63ac013421f4d8a014cbe5aa6e6b3c3e22f
Toby Wilson <wilsontcl@gmail.com>
2021-01-26 13:13:54 +0000

    removed old reducer files

--- src/actions/types.js
+++
@@ -1,5 +0,0 @@
-export const GET_FX = 'GET_FX';
-export const SET_LOADING = 'SET_LOADING';
-export const FX_ERROR = 'FX_ERROR';
-export const CHART_RESIZE = 'CHART_RESIZE';
-export const SELECT_REPORT = 'SELECT_REPORT';
--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-//import { combineReducers } from 'redux';
-import fxReducer from './fxReducer';
-
-//an object with all of the reducers
-export default fxReducer;
-// export default combineReducers({
-//   fx: fxReducer,
-// });
