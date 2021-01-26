3fe7821dbb0fccc12d94aaf99e13d628f401e6c0
Toby Wilson <wilsontcl@gmail.com>
2021-01-26 09:38:03 +0000

    with configureStore

--- src/store.js
+++ src/store.js
@@ -1,35 +1,35 @@
-// import { configureStore } from '@reduxjs/toolkit';
-// import fxReducer from './slices/fxSlice';
+import { configureStore } from '@reduxjs/toolkit';
+import fxReducer from './slices/fxSlice';
 
-// export default configureStore({
-//   reducer: {
-//     fx: fxReducer,
-//   },
-// });
+export default configureStore({
+  reducer: {
+    fx: fxReducer,
+  },
+});
 
-import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import thunk from 'redux-thunk';
-import rootReducer from './reducers';
-//import rootReducer from './slices/fxSlice';
+// import { createStore, applyMiddleware } from 'redux';
+// import { composeWithDevTools } from 'redux-devtools-extension';
+// import thunk from 'redux-thunk';
+// import rootReducer from './reducers';
+// //import rootReducer from './slices/fxSlice';
 
-const initialState = {
-  report: 'Test',
-  fxPair: 'GBPUSD',
-  chartData: null,
-  chartWidth: 600,
-  chartHeight: 400,
-  current: null,
-  loading: false,
-  error: null,
-};
+// const initialState = {
+//   report: 'Test',
+//   fxPair: 'GBPUSD',
+//   chartData: null,
+//   chartWidth: 600,
+//   chartHeight: 400,
+//   current: null,
+//   loading: false,
+//   error: null,
+// };
 
-const middleware = [thunk];
+// const middleware = [thunk];
 
-const store = createStore(
-  rootReducer,
-  initialState,
-  composeWithDevTools(applyMiddleware(...middleware))
-);
+// const store = createStore(
+//   rootReducer,
+//   initialState,
+//   composeWithDevTools(applyMiddleware(...middleware))
+// );
 
-export default store;
+// export default store;
