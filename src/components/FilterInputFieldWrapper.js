5f0fa242ec2ba72b345bb5f2dada1a662aa7125d
Toby Wilson <wilsontcl@gmail.com>
2021-01-26 13:10:57 +0000

    fxActions separated out

--- src/components/FilterInputFieldWrapper.js
+++ src/components/FilterInputFieldWrapper.js
@@ -1,46 +1,30 @@
 import React from 'react';
-
 import { useSelector, useDispatch } from 'react-redux';
-
-//import { getFx, fxPairValue, reportConfigValue } from '../slices/fxSlice';
-import * as fxActions from '../slices/fxSlice';
-
+import * as fxActions from '../slices/fxActions';
 import FilterInputField from './FilterInputField';
-
-//import { getConfig } from '../utils/getConfig';
 
 const FilterInputFieldWrapper = ({ report }) => {
   const dispatch = useDispatch();
 
-  //get report filters
-  //const reportConfig = useSelector(reportConfigValue); //need tomake this dynamic so
-  //const inputFieldArray = reportConfig.filters; //getConfig(report, 'filters');
+  //get report filters from config and add actual state and function references
   const inputFieldArray = useSelector((state) =>
     state.fx.reportConfig.filters.map((filter) => {
-      //const addRefs = { inputFieldValueRef: state['FxPair']}
-      console.log(filter.onChangeFunc);
       const func = fxActions[filter.onChangeFunc];
       const newFilterObj = {
         ...filter,
         inputFieldValueRef: state.fx[filter.inputFieldValue],
-        //onChangeFuncRef: func,
-        onChangeFuncRef: (fxPair) => dispatch(func(fxPair)),
+        onChangeFuncRef: (...args) => dispatch(func(...args)),
       };
-      console.log(newFilterObj);
-      console.log(typeof func);
       return newFilterObj;
     })
   );
 
-  //const fxPair = useSelector((state) => state['FxPair']);
-  //const fxPair = useSelector(fxPairValue); //need tomake this dynamic so
-
+  //generate individual filter components
   const result = inputFieldArray.map((filter) => {
     const labelText = filter.labelText;
     const optionValues = filter.optionValues;
-    const onChangeFunc = filter.onChangeFuncRef; //(fxPair) => dispatch(getFx(fxPair)); //rest[filter.onChangeFunc];
-    //const onChangeFunc = (fxPair) => dispatch(getFx(fxPair)); //rest[filter.onChangeFunc];
-    const inputFieldValue = filter.inputFieldValueRef; //fxPair; //rest[filter.inputFieldValue];
+    const onChangeFunc = filter.onChangeFuncRef;
+    const inputFieldValue = filter.inputFieldValueRef;
     const id = filter.id;
 
     return (
---
+++ src/slices/fxActions.js
@@ -0,0 +1,9 @@
+//reexport actions so they can be imported on their own
+export {
+  GET_FX,
+  SET_LOADING,
+  FX_ERROR,
+  CHART_RESIZE,
+  SELECT_REPORT,
+  getFx,
+} from './fxSlice';
