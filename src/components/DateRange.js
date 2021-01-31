f90047bfec32b8987af2ccd6e7b248ca76db0640
Toby Wilson <wilsontcl@gmail.com>
2021-01-31 18:38:13 +0000

    defaulting dates correctly

--- src/components/DateRange.js
+++ src/components/DateRange.js
@@ -2,142 +2,88 @@ import React, { useRef, useEffect } from 'react';
 import noUiSlider from 'materialize-css/extras/noUiSlider/nouislider.min.js';
 import 'materialize-css/extras/noUiSlider/nouislider.css';
 import wNumb from 'wnumb/wNumb.js';
-import { formatDate as myFormatDate } from '../utils/utils';
-
-function timestamp(str) {
-  return new Date(str).getTime();
-}
-
-// Create a list of day and month names.
-var weekdays = [
-  'Sunday',
-  'Monday',
-  'Tuesday',
-  'Wednesday',
-  'Thursday',
-  'Friday',
-  'Saturday',
-];
-
-var months = [
-  'January',
-  'February',
-  'March',
-  'April',
-  'May',
-  'June',
-  'July',
-  'August',
-  'September',
-  'October',
-  'November',
-  'December',
-];
-
-// Append a suffix to dates.
-// Example: 23 => 23rd, 1 => 1st.
-function nth(d) {
-  if (d > 3 && d < 21) return 'th';
-  switch (d % 10) {
-    case 1:
-      return 'st';
-    case 2:
-      return 'nd';
-    case 3:
-      return 'rd';
-    default:
-      return 'th';
-  }
-}
-
-// Create a string representation of the date.
-function formatDate(date) {
-  return (
-    weekdays[date.getDay()] +
-    ', ' +
-    date.getDate() +
-    nth(date.getDate()) +
-    ' ' +
-    months[date.getMonth()] +
-    ' ' +
-    date.getFullYear()
-  );
-}
+import { formatDate, timestamp, noUIFormatDate } from '../utils/utils';
 
 const DateRange = ({ labelText, inputFieldValue, onChangeFunc }) => {
   const sliderRef = useRef(null);
   const eventStartRef = useRef(null);
   const eventEndRef = useRef(null);
+  //console.log(`onChangeFunc is ${onChangeFunc}`);
+
+  const startDate = !inputFieldValue[0]
+    ? timestamp('2019-01-01')
+    : timestamp(inputFieldValue[0]);
+
+  let today = new Date();
+  let lastBusinessDay = today.setDate(today.getDate());
+  lastBusinessDay = timestamp(formatDate(lastBusinessDay));
+
+  let endDate = !inputFieldValue[1]
+    ? lastBusinessDay
+    : timestamp(inputFieldValue[1]);
 
   useEffect(() => {
-    noUiSlider.create(sliderRef.current, {
-      // Create two timestamps to define a range.
-      range: {
-        min: timestamp('2019'),
-        max: timestamp('2020'),
-      },
+    if (!sliderRef.current?.noUiSlider) {
+      noUiSlider.create(sliderRef.current, {
+        // Create two timestamps to define a range.
+        range: {
+          min: timestamp('2019'),
+          max: endDate,
+        },
 
-      // Steps of one week
-      step: 7 * 24 * 60 * 60 * 1000,
+        // Steps of one week
+        step: 7 * 24 * 60 * 60 * 1000,
 
-      // Two more timestamps indicate the handle starting positions.
-      start: [timestamp('2019'), timestamp('2020')],
+        // Two more timestamps indicate the handle starting positions.
+        start: [timestamp('2019'), timestamp('2020')],
 
-      tooltips: false,
-      connect: true,
+        tooltips: false,
+        connect: true,
 
-      // No decimals
-      format: wNumb({
-        decimals: 0,
-      }),
-    });
+        // No decimals
+        format: wNumb({
+          decimals: 0,
+        }),
+      });
 
-    //sliderRef.current.noUiSlider.set();
+      //sliderRef.current.noUiSlider.set();
 
-    var dateValues = [eventStartRef.current, eventEndRef.current];
+      var dateValues = [eventStartRef.current, eventEndRef.current];
 
-    sliderRef.current.noUiSlider.on('update', function (values, handle) {
-      dateValues[handle].innerHTML = formatDate(new Date(+values[handle]));
-    });
+      sliderRef.current.noUiSlider.on('update', function (values, handle) {
+        dateValues[handle].innerHTML = noUIFormatDate(
+          new Date(+values[handle])
+        );
+      });
 
-    function datesUpdated(
-      values,
-      handle,
-      unencoded,
-      tap,
-      positions,
-      noUiSlider
-    ) {
-      // values: Current slider values (array);
-      // handle: Handle that caused the event (number);
-      // unencoded: Slider values without formatting (array);
-      // tap: Event was caused by the user tapping the slider (boolean);
-      // positions: Left offset of the handles (array);
-      // noUiSlider: slider public Api (noUiSlider);
-      const newStartDate = myFormatDate(unencoded[0]);
-      const newEndDate = myFormatDate(unencoded[1]);
-      onChangeFunc([newStartDate, newEndDate]);
+      function datesUpdated(
+        values,
+        handle,
+        unencoded,
+        tap,
+        positions,
+        noUiSlider
+      ) {
+        // values: Current slider values (array);
+        // handle: Handle that caused the event (number);
+        // unencoded: Slider values without formatting (array);
+        // tap: Event was caused by the user tapping the slider (boolean);
+        // positions: Left offset of the handles (array);
+        // noUiSlider: slider public Api (noUiSlider);
+        const newStartDate = formatDate(unencoded[0]);
+        const newEndDate = formatDate(unencoded[1]);
+        onChangeFunc([newStartDate, newEndDate]);
+      }
+
+      // Binding signature
+      sliderRef.current.noUiSlider.on('end', datesUpdated);
     }
-
-    // Binding signature
-    sliderRef.current.noUiSlider.on('end', datesUpdated);
-  }, [onChangeFunc]);
+  }, [onChangeFunc, endDate]);
 
   useEffect(() => {
-    const startDate = !inputFieldValue[0]
-      ? timestamp('2019-01-01')
-      : timestamp(inputFieldValue[0]);
-
-    let today = new Date();
-    let lastBusinessDay = today.setDate(today.getDate());
-    lastBusinessDay = timestamp(myFormatDate(lastBusinessDay));
-    let endDate = !inputFieldValue[1]
-      ? lastBusinessDay
-      : timestamp(inputFieldValue[1]);
-
     //console.log([startDate, endDate]);
     sliderRef.current.noUiSlider.set([startDate, endDate]);
-  }, [inputFieldValue]);
+  }, [startDate, endDate]);
 
   return (
     <div className='slider-container'>
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -26,3 +26,63 @@ export function formatDate(date) {
 
   return [year, month, day].join('-');
 }
+
+export function timestamp(str) {
+  return new Date(str).getTime();
+}
+
+// Create a list of day and month names.
+var weekdays = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+];
+
+var months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+
+// Append a suffix to dates.
+// Example: 23 => 23rd, 1 => 1st.
+export function nth(d) {
+  if (d > 3 && d < 21) return 'th';
+  switch (d % 10) {
+    case 1:
+      return 'st';
+    case 2:
+      return 'nd';
+    case 3:
+      return 'rd';
+    default:
+      return 'th';
+  }
+}
+
+// Create a string representation of the date.
+export function noUIFormatDate(date) {
+  return (
+    weekdays[date.getDay()] +
+    ', ' +
+    date.getDate() +
+    nth(date.getDate()) +
+    ' ' +
+    months[date.getMonth()] +
+    ' ' +
+    date.getFullYear()
+  );
+}
